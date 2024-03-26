592ada749e342bb69f4326e2732c36d6c2ae7e5a
willkodes <willkbui.codes@gmail.com>
2024-03-26 18:00:24 -0400

    added javascript
    just some javascript to the quizzes on my pages

---
+++ javascript/SecurityQuiz.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent form submission
+
+    
+        const answers = {
+            question1: 'html',
+            question2: 'tornado',
+            question3: 'bachleor'
+        };
+        
+        const totalQuestions = Object.keys(answers).length;
+
+        for (const questionId in answers) {
+            const selectedOption = document.querySelector(`input[name=${questionId}]:checked`);
+            if (selectedOption) {
+                const selectedValue = selectedOption.value;
+                const correctAnswer = answers[questionId];
+
+                if (selectedValue === correctAnswer) {
+                    selectedOption.parentElement.classList.add('correct');
+                } else {
+                    selectedOption.parentElement.classList.add('incorrect');
+                    const correctOption = document.querySelector(`input[value=${correctAnswer}]`);
+                    correctOption.parentElement.classList.add('correct');
+                }
+            }
+        }
+
+    });
+});
